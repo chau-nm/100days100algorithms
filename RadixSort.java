7870aba0aa1a4428b7d238370dc6515e4f661f85
chau-nm <chaunm.146.2001@gmail.com>
2024-10-12 14:44:56 +0700

    Day 17

---
+++ RadixSort.java
@@ -0,0 +1,51 @@
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public int[] radixSort(int[] arr) {
+        int max = getMax(arr);
+        
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSort(arr, exp);
+        }
+        return arr;
+    }
+
+    private void countingSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    private int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        RadixSort radixSort = new RadixSort();
+        System.out.println(Arrays.toString(radixSort.radixSort(arr)));
+    }
+}
